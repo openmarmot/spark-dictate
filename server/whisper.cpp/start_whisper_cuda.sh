11202a2824755dd3864cba3e28342ee0682760de
andrew <andrew@openmarmot.com>
2026-05-15 22:32:53 -0700

    update

--- server/whisper.cpp/start_whisper_cuda.sh
+++ server/whisper.cpp/start_whisper_cuda.sh
@@ -15,12 +15,13 @@ if [ ! -d "whisper.cpp" ]; then
     cd ./whisper.cpp
     cmake -B build-cuda -DGGML_CUDA=ON
     cmake --build build-cuda -j
+
+    printf "[I] Downloading Whisper model...\n"
+    ./models/download-ggml-model.sh large-v3-turbo-q8_0 > /dev/null 2>&1
+    cd ..
 else
     printf "[I] whisper.cpp already exists, skipping build\n"
 fi
-
-printf "[I] Downloading Whisper model...\n"
-./models/download-ggml-model.sh large-v3-turbo-q8_0 > /dev/null 2>&1
 
 printf "Starting Whisper.. \n"
 ./whisper.cpp/build-cuda/bin/whisper-server -m ./whisper.cpp/models/ggml-large-v3-turbo-q8_0.bin --host 0.0.0.0 --port 8025 --inference-path /v1/audio/transcriptions
